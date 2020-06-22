258e15c2718d36ec50d285e445653b5d80f7d00b
AdrianValdes <adrianvaldeshdz@.gmail.com>
2020-06-22 22:57:39 +0200

    Added first reducers

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -2,11 +2,10 @@ import { getInitialData } from '../utils/api';
 import { receiveUsers } from './users';
 import { receivePolls } from './polls';
 import { setAuthedUser } from './authedUser';
-// This is going to make an ajax request,
-// so it is going to be a thunk.
 
 const AUTHED_ID = 'sarah_edo';
-
+// This is going to make an ajax request,
+// so it is going to be a thunk.
 export function handleInitialData() {
   return (dispatch) => {
     return getInitialData().then(({ users, polls }) => {
